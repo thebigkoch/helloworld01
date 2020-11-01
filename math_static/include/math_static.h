32cbd5e6f337e33c3d9d2f26d41e4a7a95a36f3a
jimkoch <jimkoch@gmail.com>
2020-10-31 22:28:56 -0400

    HelloWorld: Add a static library using CMake.

---
+++ math_static/include/math_static.h
@@ -0,0 +1,11 @@
+namespace thebigkoch
+{
+class math_static
+{
+public:
+    static int sum( int a, int b );
+    static int difference( int a, int b );
+    static int product( int a, int b );
+    static int quotient( int a, int b );
+};
+}
